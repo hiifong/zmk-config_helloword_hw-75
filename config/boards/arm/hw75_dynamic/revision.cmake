6803073bce1f2876dd5f1cb21efdeffc0be14290
XiNGRZ <hi@xingrz.me>
2025-12-19 16:39:16 +0800

    boards: Migrate to HWMv2 board layout

--- config/boards/arm/hw75_dynamic/revision.cmake
+++
@@ -1,7 +0,0 @@
-# Copyright (c) 2023 XiNGRZ
-# SPDX-License-Identifier: MIT
-
-board_check_revision(
-    FORMAT LETTER
-    DEFAULT_REVISION B
-)
--- config/boards/arm/hw75_keyboard/revision.cmake
+++
@@ -1,7 +0,0 @@
-# Copyright (c) 2022-2023 XiNGRZ
-# SPDX-License-Identifier: MIT
-
-board_check_revision(
-    FORMAT MAJOR.MINOR.PATCH
-    DEFAULT_REVISION 1.2.0
-)
